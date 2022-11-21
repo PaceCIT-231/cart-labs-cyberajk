fef43ee650a292fa57d33bc5eb38ae9ad165dd09
Andrew <ak34776p@pace.edu>
2022-11-21 13:51:07 -0500

    darkmodeplus

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,6 +14,10 @@ function addToCart(cookie) {
    //add 1 to the itemCount variable
    itemCount = itemCount+1
     console.log(itemCount)
+    
+    document.getElementById("cartItems").innerHTML= itemCount
+
+        
 
    //add the correct price to the currentPrice variable
    if (cookie == 'peanut butter') {
@@ -28,6 +32,10 @@ function addToCart(cookie) {
     else if (cookie == 'chocolate chip') {
     currentPrice = currentPrice + 25
     }
+
+document.querySelector(".hoverText").innerHTML = currentPrice
+
+
 console.log(currentPrice)
 }
 
@@ -35,6 +43,24 @@ function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
     
-    window.alert(`You have ${itemCount} items that will cost $${currentPrice}.  Thank you for shopping with us!`)
+    //window.alert(`You have ${itemCount} items that will cost $${currentPrice}.  Thank you for shopping with us!`)
+
+    window.prompt(`Please provide your name and address \nThank you for shopping with us!`)
+
+    currentPrice = 0
+    itemCount = 0
+
+    document.getElementById("cartItems").innerHTML= itemCount
+    document.querySelector(".hoverText").innerHTML = currentPrice
 
 }
+
+function darkMode() {
+	// add your code here
+    
+    document.querySelector("body").style.backgroundColor = "black"
+    document.querySelector("main").style.color = "white"
+    document.querySelector("h1").style.color = "white"
+
+}
+
